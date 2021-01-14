f12f5bd269671dbc07e26ad4e0b763f0177e0d01
yihuansone <yihuansone@gmail.com>
2021-01-14 13:00:07 +0800

    New

---
+++ app/src/main/java/com/example/uploadpictures/AfterPassword.java
@@ -0,0 +1,14 @@
+package com.example.uploadpictures;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AfterPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_password);
+    }
+}
